6fa484f0285efc9a4db46830b378da29f4f7d834
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-08-12 11:08:32 -0400

    :books: add comments

--- src/EcologicalNetworksPlots.jl
+++ src/EcologicalNetworksPlots.jl
@@ -4,13 +4,17 @@ using EcologicalNetworks
 using Plots
 using RecipesBase
 
+# Various layout manipulation functions
 include(joinpath(".", "utilities.jl"))
 
+# Types for layout positioning
 include(joinpath(".", "types.jl"))
 export NodePosition
 
+# Force-directed layout
 include(joinpath(".", "forcedirected.jl"))
 
+# Recipes
 include(joinpath(".", "recipes.jl"))
 
 end # module
