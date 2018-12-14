fddf96fe04e01cb9232bd5f3a0c1e28dc4295b5c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-14 16:56:37 +0000

    build based on 482cc45

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Index",
+    "title": "Index",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#My-first-plot!-1",
+    "page": "Index",
+    "title": "My first plot!",
+    "category": "section",
+    "text": "using EcologicalNetworks\nusing EcologicalNetworksPlots"
+},
+
+]}
