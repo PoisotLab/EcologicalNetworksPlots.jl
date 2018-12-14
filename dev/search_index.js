1081490f46c1f39d21d74b10d2aa0809d929c3c0
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-14 18:55:32 +0000

    build based on 3b1ad7c

--- dev/search_index.js
+++ dev/search_index.js
@@ -16,4 +16,84 @@ var documenterSearchIndex = {"docs": [
     "text": "using EcologicalNetworks\nusing EcologicalNetworksPlots"
 },
 
+{
+    "location": "#EcologicalNetworksPlots.initial_random_layout",
+    "page": "Index",
+    "title": "EcologicalNetworksPlots.initial_random_layout",
+    "category": "function",
+    "text": "Random disposition of nodes\n\n\n\n\n\n"
+},
+
+{
+    "location": "#EcologicalNetworksPlots.initial_bipartite_layout",
+    "page": "Index",
+    "title": "EcologicalNetworksPlots.initial_bipartite_layout",
+    "category": "function",
+    "text": "Random disposition of nodes on two levels for bipartite networks\n\n\n\n\n\n"
+},
+
+{
+    "location": "#EcologicalNetworksPlots.initial_foodweb_layout",
+    "page": "Index",
+    "title": "EcologicalNetworksPlots.initial_foodweb_layout",
+    "category": "function",
+    "text": "Random disposition of nodes on trophic levels for food webs\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Initial-layouts-1",
+    "page": "Index",
+    "title": "Initial layouts",
+    "category": "section",
+    "text": "initial_random_layout\ninitial_bipartite_layout\ninitial_foodweb_layout"
+},
+
+{
+    "location": "#EcologicalNetworksPlots.graph_layout!",
+    "page": "Index",
+    "title": "EcologicalNetworksPlots.graph_layout!",
+    "category": "function",
+    "text": "Graph\n\n\n\n\n\n"
+},
+
+{
+    "location": "#EcologicalNetworksPlots.bipartite_layout!",
+    "page": "Index",
+    "title": "EcologicalNetworksPlots.bipartite_layout!",
+    "category": "function",
+    "text": "Bipartite\n\n\n\n\n\n"
+},
+
+{
+    "location": "#EcologicalNetworksPlots.foodweb_layout!",
+    "page": "Index",
+    "title": "EcologicalNetworksPlots.foodweb_layout!",
+    "category": "function",
+    "text": "Foodweb\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Force-directed-layouts-1",
+    "page": "Index",
+    "title": "Force-directed layouts",
+    "category": "section",
+    "text": "graph_layout!\nbipartite_layout!\nfoodweb_layout!"
+},
+
+{
+    "location": "#EcologicalNetworksPlots.NodePosition",
+    "page": "Index",
+    "title": "EcologicalNetworksPlots.NodePosition",
+    "category": "type",
+    "text": "NodePosition\n\nRepresents the positon and velocity of a node during force directed layouts. The fields are x and y for position, and vx and vy for their relative velocity.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Types-1",
+    "page": "Index",
+    "title": "Types",
+    "category": "section",
+    "text": "NodePosition"
+},
+
 ]}
