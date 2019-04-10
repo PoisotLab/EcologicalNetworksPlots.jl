8dc4524a04e9a3641afdc00add56119c8a8142b6
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-04-09 23:23:00 -0400

    :books: change docstrings

--- src/types.jl
+++ src/types.jl
@@ -1,7 +1,7 @@
 """
-NodePosition
+    NodePosition
 
-Represents the positon and velocity of a node during force directed layouts. The
+Represents the position and velocity of a node during force directed layouts. The
 fields are `x` and `y` for position, and `vx` and `vy` for their relative
 velocity.
 """
