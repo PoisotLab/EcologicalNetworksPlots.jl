60454bda256cc17e357651372f163737ad83dae5
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-04-11 10:41:55 -0400

    :hand: export circular layout

--- src/circular.jl
+++ src/circular.jl
@@ -10,6 +10,20 @@ function angle(n::NodePosition)
     return θ
 end
 
-position!(LA::CircularLayout, L::Dict{K,NodePosition}, N::T) where {T <: AbstractBipartiteNetwork} where {K}
-    
+position!(LA::CircularLayout, L::Dict{K,NodePosition}, N::T) where {T <: AbstractEcologicalNetwork} where {K}
+    S = richness(N)
+    for (i, n1) in enumerate(species(N))
+        if i < S
+            n = L[n1]
+            θ = n.r*2π/S
+            sx, sy = cos(θ), sin(θ)
+            nei = Set{last(eltype(N))}[]
+            if n1 ∈ species(N; dims=2)
+                nei = union(nei, N[:,n1])
+            end
+            if n1 ∈ species(N; dims=1)
+                nei = union(nei, N[n1,:])
+            end
+        end
+    end
 end
--- src/types.jl
+++ src/types.jl
@@ -43,3 +43,11 @@ This type is used to generate an initial layout, where the nodes are
 placed at random.
 """
 struct RandomInitialLayout end
+
+"""
+    CircularInitialLayout
+
+This type is used to generate an initial layout, where the nodes are
+placed at random along a circle.
+"""
+struct CircularInitialLayout end
