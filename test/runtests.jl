dc7b19eeef662140de0414c2b08be2ab6721fcef
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-04-11 08:27:59 -0400

    :green_heart: correct network in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,9 @@ Unes = web_of_life("M_SD_033")
 
 for al in [true, false], re in [true, false]
     I = initial(BipartiteInitialLayout, Unes)
-    position!(NestedBipartiteLayout(al, re, 0.6), I, U)
-    plot(I, U, aspectratio=1)
-    scatter!(I, U, bipartite=true)
+    position!(NestedBipartiteLayout(al, re, 0.6), I, Unes)
+    plot(I, Unes, aspectratio=1)
+    scatter!(I, Unes, bipartite=true)
     isal = al ? "aligned" : "free"
     isre = re ? "relative" : "spread"
     savefig(joinpath("..", "gallery", "bipartite-nested-$(isal)-$(isre).png"))
