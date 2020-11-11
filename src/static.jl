5331213f7d614c3781c1d16fef6b0604c0593eed
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-11-10 20:22:14 -0500

    remove the extra declaration for unravelled

--- src/static.jl
+++ src/static.jl
@@ -15,7 +15,12 @@ struct UnravelledLayout{TX<:Function,TY<:Function}
     y::TY
 end
 
-UnravelledLayout() = UnravelledLayout(omnivory, fractional_trophic_level)
+"""
+    UnravelledLayout(;x=omnivory, y=fractional_trophic_level)
+
+By default, the two functions used are omnivory and fractional trophic level,
+but this can be changed.
+"""
 UnravelledLayout(;x=omnivory, y=fractional_trophic_level) = UnravelledLayout(x, y)
 
 """
