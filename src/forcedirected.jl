0b5ea6925010ffaac6df9896d99256bfea71ef34
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-11-21 20:04:09 -0500

    ⬜ indentation

--- src/forcedirected.jl
+++ src/forcedirected.jl
@@ -35,55 +35,55 @@ ForceDirectedLayout(k::Float64) = ForceDirectedLayout(k=(k,k))
 Stops the movement of a node position.
 """
 function stop!(n::NodePosition)
-  n.vx = 0.0
-  n.vy = 0.0
+    n.vx = 0.0
+    n.vy = 0.0
 end
 
 """
 Repel two nodes
 """
 function repel!(LA::T, n1::NodePosition, n2::NodePosition, fr) where {T <: ForceDirectedLayout}
-  δx = n1.x - n2.x
-  δy = n1.y - n2.y
-  Δ = sqrt(δx^2.0+δy^2.0)
-  Δ = Δ == 0.0 ? 0.0001 : Δ
-  if LA.move[1]
-    n1.vx = n1.vx + δx/Δ*fr(Δ)
-    n2.vx = n2.vx - δx/Δ*fr(Δ)
-  end
-  if LA.move[2] # Do we need to move y here?
-    n1.vy = n1.vy + δy/Δ*fr(Δ)
-    n2.vy = n2.vy - δy/Δ*fr(Δ)
-  end
+    δx = n1.x - n2.x
+    δy = n1.y - n2.y
+    Δ = sqrt(δx^2.0+δy^2.0)
+    Δ = Δ == 0.0 ? 0.0001 : Δ
+    if LA.move[1]
+        n1.vx = n1.vx + δx/Δ*fr(Δ)
+        n2.vx = n2.vx - δx/Δ*fr(Δ)
+    end
+    if LA.move[2] # Do we need to move y here?
+        n1.vy = n1.vy + δy/Δ*fr(Δ)
+        n2.vy = n2.vy - δy/Δ*fr(Δ)
+    end
 end
 
 """
 Attract two connected nodes
 """
 function attract!(LA::T, n1::NodePosition, n2::NodePosition, fa) where {T <: ForceDirectedLayout}
-  δx = n1.x - n2.x
-  δy = n1.y - n2.y
-  Δ = sqrt(δx^2.0+δy^2.0)
-  Δ = Δ == 0.0 ? 0.0001 : Δ
-  if LA.move[1]
-    n1.vx = n1.vx - δx/Δ*fa(Δ)
-    n2.vx = n2.vx + δx/Δ*fa(Δ)
-  end
-  if LA.move[2]
-    n1.vy = n1.vy - δy/Δ*fa(Δ)
-    n2.vy = n2.vy + δy/Δ*fa(Δ)
-  end
+    δx = n1.x - n2.x
+    δy = n1.y - n2.y
+    Δ = sqrt(δx^2.0+δy^2.0)
+    Δ = Δ == 0.0 ? 0.0001 : Δ
+    if LA.move[1]
+        n1.vx = n1.vx - δx/Δ*fa(Δ)
+        n2.vx = n2.vx + δx/Δ*fa(Δ)
+    end
+    if LA.move[2]
+        n1.vy = n1.vy - δy/Δ*fa(Δ)
+        n2.vy = n2.vy + δy/Δ*fa(Δ)
+    end
 end
 
 """
 Update the position of a node
 """
 function update!(n::NodePosition) where {T <: ForceDirectedLayout}
-  Δ = sqrt(n.vx^2.0+n.vy^2.0)
-  Δ = Δ == 0.0 ? 0.0001 : Δ
-  n.x += n.vx/Δ*min(Δ, 0.01)
-  n.y += n.vy/Δ*min(Δ, 0.01)
-  stop!(n)
+    Δ = sqrt(n.vx^2.0+n.vy^2.0)
+    Δ = Δ == 0.0 ? 0.0001 : Δ
+    n.x += n.vx/Δ*min(Δ, 0.01)
+    n.y += n.vy/Δ*min(Δ, 0.01)
+    stop!(n)
 end
 
 """
@@ -94,33 +94,34 @@ take some time to converge, it may be useful to stop every 500 iterations to
 have a look at the results.
 """
 function position!(LA::ForceDirectedLayout, L::Dict{K,NodePosition}, N::T) where {T <: EcologicalNetworks.AbstractEcologicalNetwork} where {K}
-  fa(x) = (x^2.0)/LA.ka # Default attraction function
-  fr(x) = (LA.kr^2.0)/x # Default repulsion function
-  for (i, s1) in enumerate(species(N))
-    n1 = L[s1]
-    stop!(n1)
-    for (j, s2) in enumerate(species(N))
-      n2 = L[s2]
-      if j != i
-        repel!(LA, n1, n2, fr)
-      end
+    fa(x) = (x^2.0)/LA.ka # Default attraction function
+    fr(x) = (LA.kr^2.0)/x # Default repulsion function
+    
+    for (i, s1) in enumerate(species(N))
+        n1 = L[s1]
+        stop!(n1)
+        for (j, s2) in enumerate(species(N))
+            n2 = L[s2]
+            if j != i
+                repel!(LA, n1, n2, fr)
+            end
+        end
     end
-  end
-
-  for int in interactions(N)
-    n1, n2 = L[int.from], L[int.to]
-    attract!(LA, n1, n2, fa)
-  end
-
-  if LA.gravity > 0.0
-    plotcenter = NodePosition(0.0, 0.0, 0.0, 0.0)
+    
+    for int in interactions(N)
+        n1, n2 = L[int.from], L[int.to]
+        attract!(LA, n1, n2, fa)
+    end
+    
+    if LA.gravity > 0.0
+        plotcenter = NodePosition(0.0, 0.0, 0.0, 0.0)
+        for s in species(N)
+            attract!(LA, L[s], plotcenter, (x) -> LA.gravity*fa(x))
+        end
+    end
+    
     for s in species(N)
-      attract!(LA, L[s], plotcenter, (x) -> LA.gravity*fa(x))
+        update!(L[s])
     end
-  end
-
-  for s in species(N)
-    update!(L[s])
-  end
-
+    
 end
